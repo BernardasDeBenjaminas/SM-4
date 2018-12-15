620eda38b8282d3d25d12ae45ea5d30160d48e1a
Benas Bagdanovas <benas.bagdanovas@gmail.com>
2018-12-15 21:10:54 +0200

    Add 1 dimensional Gaussian method. Rename file to better reflect contents.

--- NumericalIntegration.m
+++ NumericalIntegration.m
@@ -1,12 +1,13 @@
-classdef TrapezoidalRule
+classdef NumericalIntegration
 
     % Constructor
     methods
-        function this = TrapezoidalRule ()
+        function this = NumericalIntegration ()
             p = 2; % level of accuracy
             a = 1; % interval start
             b = 3; % interval end
-            N = [10, 20, 40, 80];
+            % N = [10, 20, 40, 80];
+            N = [4, 8, 16, 32];
             f = @(x) 1./x;
 
             this.Solve(p, a, b, N, f);
@@ -16,22 +17,44 @@ classdef TrapezoidalRule
     % Public methods
     methods
         function Solve (this, p, a, b, N, f)
+            disp(' ');
+            disp('--------------------------------------');
+            disp('--------------- PRECISE --------------');
+            disp('--------------------------------------');
             I = integral(f, a, b);
+            disp(num2str(I));
             
+            disp(' ');
+            disp('--------------------------------------');
+            disp('-------------- TRAPEZIAN -------------');
+            disp('--------------------------------------');
             for i = 1 : length(N)
-                S_N = this.GetTrapezoidArea(a, b, N(i), f);
-                S_N2 = this.GetTrapezoidArea(a, b, N(i) / 2, f);
+                S_N = this.GetTrapezoid(a, b, N(i), f);
+                S_N2 = this.GetTrapezoid(a, b, N(i) / 2, f);
                 runge = abs(S_N - S_N2) / (2^p - 1);
                 error = abs(I - S_N);
 
                 this.PrintStepResults(N(i), S_N, error, runge);                    
+            end
+
+            disp(' ');
+            disp('--------------------------------------');
+            disp('--------------- GAUSSIAN -------------');
+            disp('--------------------------------------');
+            for i = 1 : length(N)
+                S_N = this.GetGaussian(a, b, N(i), f);
+                S_N2 = this.GetTrapezoid(a, b, N(i) / 2, f);
+                runge = abs(S_N - S_N2) / (2^p - 1);
+                error = abs(I - S_N);
+
+                this.PrintStepResults(N(i), S_N, error, runge);
             end
         end
     end
 
     % Private methods
     methods (Access = private)
-        function res = GetTrapezoidArea (this, a, b, N, f)
+        function res = GetTrapezoid (this, a, b, N, f)
             step = (b - a) / N;
             range = a : step : b;
 
@@ -45,6 +68,18 @@ classdef TrapezoidalRule
             res = total;
         end
 
+        function res = GetGaussian (this, a, b, N, f)
+            step = (b - a) / N;
+            range = a : step : b;
+
+            total = 0;
+            for i = 1 : length(range) - 1
+                x = range(i) + range(i + 1);
+                total = total + f(x / 2) * step;
+            end
+
+            res = total;
+        end
 
         function res = PadString (this, message, width)
 			differ = width - length(message);
